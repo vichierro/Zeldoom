dc86e8ec66194db4785b9e25215523c5ac29f6c7
DaviMaia-cloud <davimaia0111@gmail.com>
2022-04-03 21:06:05 -0300

    Adição de inimigos e elementos decorativos

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private AudioSource deathSoundEffect;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        deathSoundEffect.Play();
+    }
+
+   
+}
